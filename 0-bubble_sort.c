dc7cc7d4d3c77df35bbc8c8b285c9b0e9d0fe27a
Benjamin <benjamin.alazet@gmail.com>
2023-05-22 12:58:39 +0200

    Modify bubble function and fonctions for Betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,32 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Prints an array of integers
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
  *
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0;
-    size_t j = 0;
-    size_t temp;
+	size_t i = 0;
+	size_t j = 0;
+	size_t temp;
 
-    if (size < 2 || array == NULL)
-        return;
+	if (size < 2 || array == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
