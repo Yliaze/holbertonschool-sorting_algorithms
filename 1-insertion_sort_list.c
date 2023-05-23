bfb53386d7a8a8e23b29087e997611a3ef313440
Benjamin <benjamin.alazet@gmail.com>
2023-05-23 10:02:17 +0200

    Modify insertion function for Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,14 +7,17 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+
+	listint_t *current, *insertion_point;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
+	current = (*list)->next;
 
 	while (current != NULL)
 	{
-		listint_t *insertion_point = current->prev;
+		insertion_point = current->prev;
 
 		while (insertion_point != NULL && insertion_point->n > current->n)
 		{
