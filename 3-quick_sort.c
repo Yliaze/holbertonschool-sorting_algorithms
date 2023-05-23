9b86d5a4113f9049a7107c09f2f708dd2083e5c0
Benjamin <benjamin.alazet@gmail.com>
2023-05-23 16:10:23 +0200

    function with the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,82 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers in ascending
- * order using the Selection sort algorithm
+ * partition - Effectue la partitionnement du tableau
+ * en utilisant le schéma de partition de Lomuto
  *
- * @array: The list to be printed
- * @size: the size
+ * @array: Tableau à trier
+ * @low: Index de début de la partition
+ * @high: Index de fin de la partition
+ * @size: size of the array
+ *
+ * Return: Index du pivot après le partitionnement
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot;
+	int i = low - 1, temp = 0, j = 0;
+
+	pivot = array[high];
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_helper - Fonction auxiliaire récursive
+ * pour effectuer le tri rapide
+ * en utilisant le schéma de partition de Lomuto
+ *
+ * @array: Tableau à trier
+ * @low: Index de début de la partition
+ * @high: Index de fin de la partition
+ * @size: size of the array
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high, size);
+
+		quick_sort_helper(array, low, pivot_index - 1, size);
+		quick_sort_helper(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Trie un tableau d'entiers en utilisant
+ * l'algorithme de tri rapide avec le schéma de partition
+ * de Lomuto
+ *
+ * @array: Tableau à trier
+ * @size: Taille du tableau
  */
 void quick_sort(int *array, size_t size)
 {
-    test
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_helper(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void quick_sort_helper(int *array, int low, int high, size_t size);
 
 #endif
